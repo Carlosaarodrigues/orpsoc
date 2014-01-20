70d83bdb8d1b34c80aef9f4476a7c8f82d8d6261
Jose T. de Sousa <jts@inesc-id.pt>
2014-01-20 23:37:30 +0000

    Additions to Verilator support

--- orpsoc/simulator/verilator.py
+++ orpsoc/simulator/verilator.py
@@ -15,6 +15,8 @@ class Verilator(Simulator):
         self.include_files = []
         self.include_dirs = []
         self.tb_toplevel = ""
+        self.src_type = 'C'
+        self.define_file = []
 
         if system.verilator is not None:
             self._load_dict(system.verilator)
@@ -32,10 +34,13 @@ class Verilator(Simulator):
                 self.src_files = items.get(item).split()
             elif item == 'include_files':
                 self.include_files = items.get(item).split()
-                self.include_dirs  = list(set(map(os.path.dirname, self.include_files)))
-                
+                self.include_dirs  = list(set(map(os.path.dirname, self.include_files)))              
             elif item == 'tb_toplevel':
                 self.tb_toplevel = items.get(item)
+            elif item == 'source_type':
+                self.src_type = items.get(item)
+            elif item == 'define_file':
+                self.define_file = items.get(item)
             else:
                 print("Warning: Unknown item '" + item +"' in verilator section")
 
@@ -65,12 +70,40 @@ class Verilator(Simulator):
         f = open(os.path.join(self.sim_root,self.verilator_file),'w')
 
         for include_dir in self.verilog.include_dirs:
-            f.write("+incdir+" + os.path.abspath(include_dir) + '\n')
+            if include_dir.find('jtag_vpi') == -1:
+                f.write("+incdir+" + os.path.abspath(include_dir) + '\n')
+
         for src_file in self.verilog.src_files:
-            f.write(os.path.abspath(src_file) + '\n')
+            if src_file.find('jtag_vpi') == -1:
+                f.write(os.path.abspath(src_file) + '\n')
+        f.close()
+        #convert verilog defines into C file
+        if self.define_file:
+            fV = open (os.path.join(self.sim_root,"../src",self.define_file),'r')
+            fC = open (os.path.join(os.path.dirname(os.path.join(self.sim_root,self.tb_toplevel)),os.path.splitext(os.path.basename(self.define_file))[0]+'.h'),'w')
+            fC.write("//File auto-converted the Verilog to C. converted by ORPSOC//\n")
+            fC.write("//source file --> " + os.path.join(self.sim_root,"../src",self.define_file)+"\n")
+            for line in fV:
+                Sline=line.split('`',1)
+                if len(Sline) == 1:
+                    fC.write(Sline[0])
+                else:
+                    fC.write(Sline[0]+"#"+Sline[1])
+            fC.close
+            fV.close
+
         
     def build(self):
         super(Verilator, self).build()
+        if self.src_type == 'C':
+            self.build_C()
+        elif self.src_type == 'systemC':
+            self.build_SysC()
+        else:
+            print("Source type not available. 'C' or 'systemC'")
+            exit(1)
+
+    def build_C(self):
         args = ['-c']
         args += ['-I'+s for s in self.include_dirs]
         for src_file in self.src_files:
@@ -100,6 +133,52 @@ class Verilator(Simulator):
         utils.launch('make -f Vorpsoc_top.mk Vorpsoc_top',
                      cwd=os.path.join(self.sim_root, 'obj_dir'),
                      shell=True)
+
+    def build_SysC(self):
+
+	object_files = [os.path.splitext(os.path.basename(s))[0]+'.o' for s in self.src_files]
+
+	#verilog
+	try:
+	    cmd = os.path.join(self.verilator_root,'bin','verilator') 
+	    subprocess.check_call(['bash',cmd,'--sc','--top-module', 'orpsoc_top','-f',self.verilator_file,'--exe']+
+				    [os.path.join(self.sim_root, s) for s in object_files]+[self.tb_toplevel]+self.verilator_options,
+            			    cwd = os.path.join(self.sim_root),
+	    			    stderr = open(os.path.join(self.sim_root,'verilator.log'),'w'))
+        except OSError:
+            print("Error: Command verilator not found. Make sure it is in $PATH")
+            exit(1)
+        except subprocess.CalledProcessError:
+            print("Error: Failed to compile. See " + os.path.join(self.sim_root,'verilator.log') + " for details")
+            exit(1)
+
+	 #src_files	
+	args = ['-I.']
+	args += ['-MMD']
+	args += ['-I'+s for s in self.include_dirs]
+	args += ['-Iobj_dir']
+	args += ['-I'+os.path.join(self.verilator_root,'include')]
+	args += ['-I'+os.path.join(self.verilator_root,'include', 'vltstd')]  
+	args += ['-DVL_PRINTF=printf']
+	args += ['-DVM_TRACE=1']
+	args += ['-DVM_COVERAGE=0']
+	args += [os.getenv('SYSTEMC_CXX_FLAGS')]
+	args += ['-I'+os.getenv('SYSTEMC_INCLUDE')]
+	args += ['-Wno-deprecated']
+	args += [os.getenv('SYSTEMC_CXX_FLAGS')]
+	args += ['-c']
+	args += ['-g']
+
+	for src_file in self.src_files:
+	    print("Compiling " + src_file)
+	    utils.launch('g++',args + ['-o' + os.path.splitext(os.path.basename(src_file))[0]+'.o']+ [src_file],
+				cwd=self.sim_root)
+
+	#tb_toplevel
+        utils.launch('make -f Vorpsoc_top.mk Vorpsoc_top',
+                     cwd=os.path.join(self.sim_root, 'obj_dir'),
+                     shell=True)
+
         
     def run(self, args):
         #TODO: Handle arguments parsing
